fc32a8cb6dbc0ea7ea40c905482655c01a1746ce
Yitzhak Dashevsky <yitzhakd@compiralabs.com>
2020-03-01 14:40:47 +0200

    first commit

---
+++ genetic_algorithm.py
@@ -0,0 +1,139 @@
+import random
+
+# Number of individuals
+POPULATION_SIZE = 100
+
+# Valid genes
+GENES = '''abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890, .-;:_!"#%&/()=?@${[]}'''
+
+# Target string to be generated
+TARGET = "Hello, Bro"
+
+
+class Individual(object):
+    """
+    Class representing individual in population
+    """
+
+    def __init__(self, chromosome):
+        self.chromosome = chromosome
+        self.fitness = self.calc_fitness()
+
+    @classmethod
+    def mutated_genes(self):
+        """
+        create random genes for mutation
+        :return: random genes
+        """
+        global GENES
+        return random.choice(GENES)
+
+    @classmethod
+    def create_genome(self):
+        """
+        create chromosome
+        :return:
+        """
+        global TARGET
+        genome_len = len(TARGET)
+        return [self.mutated_genes() for _ in range(genome_len)]
+
+    def calc_fitness(self):
+        """
+        Calculate fitness score, it is the number of
+        characters in string which differ from target
+        string.
+        """
+        global TARGET
+        fitness = 0
+        for gs, gt in zip(self.chromosome, TARGET):
+            if gs != gt:
+                fitness += 1
+        return fitness
+
+    def mate(self, par2):
+        """
+        Perform mating, produce offspring, crossover operator
+        :return:
+        """
+        child_chromosome = []
+        for gp1, gp2 in zip(self.chromosome, par2.chromosome):
+            # random probability
+            prob = random.random()
+
+            # if if prob is less than 0.45, insert gene from parent 1
+            if prob < 0.45:
+                child_chromosome.append(gp1)
+
+            # if prob is between 0.45 and 0.90, insert
+            # gene from parent 2
+            elif prob < 0.9:
+                child_chromosome.append(gp2)
+
+            # otherwise insert random gene(mutate),
+            # for maintaining diversity
+            else:
+                child_chromosome.append(self.mutated_genes())
+
+        # create new Individual(offspring) using
+        # generated chromosome for offspring
+        return Individual(child_chromosome)
+
+
+def main():
+    global POPULATION_SIZE
+
+    # current generation
+    generation = 1
+    found = False
+    population = []
+
+    # create initial population
+    for _ in range(POPULATION_SIZE):
+        genome = Individual.create_genome()
+        population.append(Individual(genome))
+
+    while not found:
+        # sort the population in increasing order of fitness score
+        population = sorted(population, key=lambda x: x.fitness)
+
+        # if the individual having lowest fitness score ie.
+        # 0 then we know that we have reached to the target
+        # and break the loop
+        if population[0].fitness <= 0:
+            found = True
+            break
+        # Otherwise generate new offsprings for new generation
+        new_generation = []
+
+        # Perform Elitism, that mean 10% of fittest population
+        # goes to the next generation
+        s = int((10 * POPULATION_SIZE) / 100)
+        new_generation.extend(population[:s])
+
+        # From 50% of fittest population, Individuals
+        # will mate to produce offspring
+        # TODO: 50% or 90%
+        s = int((90 * POPULATION_SIZE) / 100)
+        for _ in range(s):
+            parent1 = random.choice(population[:50])
+            parent2 = random.choice(population[:50])
+            child = parent1.mate(parent2)
+            new_generation.append(child)
+
+        population = new_generation
+
+        print("Generation: {}\tString: {}\tFitness: {}".
+              format(generation,
+                     "".join(population[0].chromosome),
+                     population[0].fitness))
+        generation += 1
+
+    print("Generation: {}\tString: {}\tFitness: {}".
+          format(generation,
+                 "".join(population[0].chromosome),
+                 population[0].fitness))
+
+
+if __name__ == '__main__':
+    main()
